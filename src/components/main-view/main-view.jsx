250bf4a11d55c4aec0c0cd243d96e92ebeac3ae6
obitown <92471740+obitown@users.noreply.github.com>
2022-02-02 16:47:15 -0800

    add axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,36 +8,21 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                {
-                    _id: 1,
-                    Title: 'Inception',
-                    Director: 'Christopher Nolan',
-                    Genre: 'Action',
-                    Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-                    ImagePath: 'https://c.tenor.com/VB72MwFAx4wAAAAC/inception-kukkendare.gif'
-                },
-
-                {
-                    _id: 2,
-                    Title: 'The Shawshank Redemption',
-                    Director: 'Frank Daradont',
-                    Genre: 'Drama',
-                    Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-                    ImagePath: 'https://c.tenor.com/X47ebguKe8wAAAAM/happy-to-see-you-andy-dufresne.gif'
-                },
-
-                {
-                    _id: 3,
-                    Title: 'Gladiator',
-                    Director: 'Ridley Scott',
-                    Genre: 'Action',
-                    Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
-                    ImagePath: 'https://c.tenor.com/CLdhkaZu4usAAAAd/thumbs-up.gif'
-                },
-            ],
+            movies: [],
             selectedMovie: null
         };
+    }
+
+    componentDidMount() {
+        axios.get('...')
+            .then(response => {
+                this.setState({
+                    movie: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     setSelectedMovie(newSelectedMovie) {
