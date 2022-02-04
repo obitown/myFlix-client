2e4142f0791fc8511e26f51fe2378b2b89e5f94b
obitown <92471740+obitown@users.noreply.github.com>
2022-02-04 12:24:42 -0700

    add reg-view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
     render() {
         const { movieData, onMovieClick } = this.props;
+
         return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}> {movieData.Title} </div>;
     }
 }
+
+MovieCard.protoTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
