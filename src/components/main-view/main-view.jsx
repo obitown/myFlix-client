9108e80c04ce358e25862f6ccf5ce803437ebee0
obitown <92471740+obitown@users.noreply.github.com>
2022-02-08 08:44:04 -0700

    remove container from main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
