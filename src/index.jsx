a4128e56211a2f06bcccb1f33b6688931014412a
obitown <92471740+obitown@users.noreply.github.com>
2022-02-21 14:01:26 -0700

    add store to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
+//redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 import { MainView } from './components/main-view/main-view';
 
 import { Container, Row } from 'react-bootstrap';
@@ -9,12 +15,17 @@ import { Container, Row } from 'react-bootstrap';
 // import statement to indicate that you need bundle './index.scss'
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
-
-            <MainView />
+            <Provider store={store}>
+                <Container>
+                    <MainView />
+                </Container>
+            </Provider>
 
         );
     }
